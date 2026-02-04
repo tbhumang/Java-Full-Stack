edf80fb11e104d7369aa76d045f4f38e25e599e0
tbhumang <umangtiwari00700@gmail.com>
2026-02-05 00:57:42 +0530

    [Umang] Program of largest of three numbers

---
+++ CoreProgrammingBasics/src/Operators/LargestofThreeNumbers.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class LargestofThreeNumbers {
+	public static void main (String args[]) {
+		int a = 10, b = 25, c = 15;
+
+		if (a >= b && a >= c)
+		    System.out.println(a);
+		else if (b >= a && b >= c)
+		    System.out.println(b);
+		else
+		    System.out.println(c);
+
+	}
+}
