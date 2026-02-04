b2c643c0d5ae788354a6d5c736b900caef2d9338
tbhumang <umangtiwari00700@gmail.com>
2026-02-05 00:26:23 +0530

    [Umang] Add a program of Simple Interest Calculator

---
+++ CoreProgrammingBasics/src/Variables/SimpleInterestCalculator.java
@@ -0,0 +1,14 @@
+package Variables;
+
+public class SimpleInterestCalculator {
+
+    public static void main(String[] args) {
+        double principal = 1000;
+        double rate = 5;
+        double time = 2;
+
+        double simpleInterest = (principal * rate * time) / 100;
+
+        System.out.println("Simple Interest = " + simpleInterest);
+    }
+}
