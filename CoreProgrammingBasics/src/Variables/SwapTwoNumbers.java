b253943678d303f41aaff07c5aa8f24344aa85f5
tbhumang <umangtiwari00700@gmail.com>
2026-02-04 23:57:37 +0530

    [Umang] : Added swap two number

---
+++ CoreProgrammingBasics/src/Variables/SwapTwoNumbers.java
@@ -0,0 +1,13 @@
+package Variables;
+
+public class SwapTwoNumbers {
+	public static void main(String args[]) {
+	int a = 10, b = 20;
+	a = a + b;
+	b = a - b;
+	a = a - b;
+
+	System.out.println(a + " " + b);
+
+}
+}
