f589ba1eaa53c6954c858c0f233368f8c1c4ec4a
tbhumang <umangtiwari00700@gmail.com>
2026-02-05 00:53:43 +0530

    [Umang] : Program for Even or Odd Number

---
+++ CoreProgrammingBasics/src/Operators/EvenorOdd.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class EvenorOdd {
+	public static void main(String args[]) {
+		int n = 10;
+
+		if (n % 2 == 0)
+		    System.out.println("Even");
+		else
+		    System.out.println("Odd");
+
+	}
+}
