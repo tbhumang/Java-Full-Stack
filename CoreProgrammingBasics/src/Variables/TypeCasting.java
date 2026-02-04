af83fc2881ddebdba25e1a8ea29405254fe18180
tbhumang <umangtiwari00700@gmail.com>
2026-02-05 00:30:19 +0530

    [Umang] : Program of Type casting

---
+++ CoreProgrammingBasics/src/Variables/TypeCasting.java
@@ -0,0 +1,14 @@
+package Variables;
+
+public class TypeCasting {
+	public static void main(String args[]) {
+		double d = 9.87;
+
+		int i = (int) d;
+		float f = (float) d;
+
+		System.out.println(i);
+		System.out.println(f);
+
+	}
+}
